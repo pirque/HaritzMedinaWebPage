e33d3a880c387fc0f143edb1a16e8544242d6ced
Haritz Medina <haritzmedina@gmail.com>
2016-12-07 16:28:58 +0100

    Translation to spanish partially done. Language is saved using cookies.

--- app/scripts/Controller.js
+++ app/scripts/Controller.js
@@ -1,9 +1,6 @@
-/**
- * Created by Haritz Medina on 28/09/2014.
- */
 'use strict';
 
-var Controller = function(){
+let Controller = function(){
 
 };
 Controller.prototype.initialize = function(){
@@ -17,23 +14,30 @@ Controller.prototype.initialize = function(){
   // Nav-bar items content control
   $('a.barItem').bind('click', function(event){
     if(HaritzMedina.model.content.pages[event.toElement.id]){
-      HaritzMedina.model.setPage(HaritzMedina.model.content.pages[event.toElement.id]);
+      HaritzMedina.model.setPage(HaritzMedina.model.getPageURI(event.toElement.id));
+    }
+  });
+
+  // Nav-bar language items content control
+  $('a.langItem').bind('click', function(event){
+    if(HaritzMedina.model.content.languages[event.toElement.id]){
+      // Set cookies and reload web
+      HaritzMedina.model.setUserLanguage(event.toElement.id);
+      window.location.reload();
     }
   });
 
   // Add event for cookies icon
   $('.cc_more_info').bind('click', function(){
-    HaritzMedina.model.setPage(HaritzMedina.model.content.pages['cookies']);
-  })
+    HaritzMedina.model.setPage(HaritzMedina.model.getPageURI('cookies'));
+  });
 
-  // TODO Load the view of the url
-  var selectedPage = window.location.hash.substr(1);
+  // Load the view of the url
+  let selectedPage = window.location.hash.substr(1);
   if($.isEmptyObject(selectedPage)){
     selectedPage = 'home';
   }
-  HaritzMedina.model.setPage(HaritzMedina.model.content.pages[selectedPage]);
+  HaritzMedina.model.setPage(HaritzMedina.model.getPageURI(selectedPage));
 
   HaritzMedina.view.randomizeBackground();
-
-
 };
