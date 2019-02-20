44a5a24bcb7a16a9b3b2272628b6c68a6d2f04fd
Haritz Medina <haritzmedina@gmail.com>
2019-02-20 17:01:58 +0100

    Improved size of backgrounds depending on screen size

--- app/scripts/View.js
+++ app/scripts/View.js
@@ -3,19 +3,32 @@
  */
 'use strict';
 
-var View = function(){
+let View = function(){
+  this.calculateBackgroundSize()
   this.randomizeBackground();
   this.currentDir = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'));
 };
 
 View.prototype.container = 'main-container';
-View.prototype.backgrounds = ['bg0.jpg', 'bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg5.jpg'];
+View.prototype.backgrounds = ['bg0.jpg', 'bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg'];
 
 View.prototype.showContent = function (htmlContent, htmlContainer) {
     $('#'+htmlContainer).html(htmlContent);
 };
 
 View.prototype.randomizeBackground = function(){
-  var randomBackground = this.backgrounds[Math.floor(Math.random()*this.backgrounds.length)];
-  document.body.style.background = 'url('+this.currentDir+'/images/'+randomBackground+') no-repeat center center fixed';
+  let randomBackground = this.backgrounds[Math.floor(Math.random()*this.backgrounds.length)];
+  document.body.style.backgroundImage = 'url(' +
+    this.currentDir + '/images/' + this.size + '/' + randomBackground + ')';
 };
+
+View.prototype.calculateBackgroundSize = function () {
+  let maxSize = Math.max(window.screen.availHeight, window.screen.availWidth)
+  if (maxSize <= 720) {
+    this.size = 'small'
+  } else if (maxSize <= 1080) {
+    this.size = 'medium'
+  } else {
+    this.size = 'big'
+  }
+}
